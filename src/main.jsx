4580ebc31d7aec2f95da789b770934ff439012b4
Gideon Ofori Addo <gideonaddoofori@gmail.com>
2022-07-15 22:24:52 +0000

    Deployed website

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { HashRouter } from 'react-router-dom'
 import App from './App'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <HashRouter>
+  <React.StrictMode>
     <App />
-  </HashRouter>
+  </React.StrictMode>
 )
